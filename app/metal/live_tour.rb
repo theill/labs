a5c4a6e98ba138d9d5115126d4eebc50ba8314a3
Peter Theill <peter@theill.com>
2009-07-08 22:43:12 +0200

    support for empty news

--- app/metal/live_tour.rb
+++ app/metal/live_tour.rb
@@ -12,14 +12,19 @@ class LiveTour
     if env["PATH_INFO"] =~ /^\/livetour/
       rsp = Net::HTTP.get_response(URI.parse('http://tour2009.tv2.dk/livedata.php/updates/id-14.ljson'))
       data = rsp.body
-      result = JSON.parse(data)
-      out = "<html><head><meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" /><meta name=\"viewport\" content=\"width=320,initial-scale=1,user-scalable=no\" /><title>Tour de France live opdateringer til mobiltelefoner</title></head><body><ul>"
-      result['updates'].each do |update|
-        out += "<li><b>#{update['title']} (#{Time.parse(update['time']).strftime('%H:%M')})</b><br />\n#{update['text']}<br /><br />\n\n</li>"
+      
+      result = JSON.parse(data) rescue ''
+      unless result.blank?
+        out = "<html><head><meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\" /><meta name=\"viewport\" content=\"width=320,initial-scale=1,user-scalable=no\" /><title>Tour de France live opdateringer til mobiltelefoner</title></head><body><ul>"
+        result['updates'].each do |update|
+          out += "<li><b>#{update['title']} (#{Time.parse(update['time']).strftime('%H:%M')})</b><br />\n#{update['text']}<br /><br />\n\n</li>"
+        end
+        out += "</ul><script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try {var pageTracker = _gat._getTracker(\"UA-617778-13\");pageTracker._trackPageview();} catch(err) {}</script>"
+        out += '</body></html>'
+      else
+        out = 'Ingen nyheder for dagens etape.'
       end
-      out += "</ul><script type=\"text/javascript\">var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));</script><script type=\"text/javascript\">try {var pageTracker = _gat._getTracker(\"UA-617778-13\");pageTracker._trackPageview();} catch(err) {}</script>"
-      out += '</body></html>'
-
+      
       [200, {"Content-Type" => "text/html"}, [out]]
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
