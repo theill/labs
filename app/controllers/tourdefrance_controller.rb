7cc2d9aa0ee2910306697e6a99987ae9288096ca
Peter Theill <peter@theill.com>
2009-07-10 20:56:12 +0200

    new id

--- app/controllers/tourdefrance_controller.rb
+++ app/controllers/tourdefrance_controller.rb
@@ -3,7 +3,7 @@ require 'net/http'
 
 class TourdefranceController < ApplicationController
   def index
-    rsp = Net::HTTP.get_response(URI.parse('http://tour2009.tv2.dk/livedata.php/updates/id-15.ljson'))
+    rsp = Net::HTTP.get_response(URI.parse('http://tour2009.tv2.dk/livedata.php/updates/id-16.ljson'))
     data = rsp.body
     
     # result = JSON.parse(data) rescue ''
