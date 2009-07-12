010c59a7f08ffba91e8fd47be27eb79537e7dfb8
Peter Theill <peter@theill.com>
2009-07-12 11:21:40 +0200

    initial view for standard mobiles

--- app/controllers/tourdefrance_controller.rb
+++ app/controllers/tourdefrance_controller.rb
@@ -3,10 +3,19 @@ require 'net/http'
 
 class TourdefranceController < ApplicationController
   def index
-    rsp = Net::HTTP.get_response(URI.parse('http://tour2009.tv2.dk/livedata.php/updates/id-16.ljson'))
+    rsp = Net::HTTP.get_response(URI.parse('http://tour2009.tv2.dk/livedata.php/updates/id-' + (Date.today.day + 5) + '.ljson'))
     data = rsp.body
     
-    # result = JSON.parse(data) rescue ''
+    @updates = ''
+    
+    result = JSON.parse(data) rescue ''
+    unless result.blank?
+      @updates = ""
+      result['updates'].each do |update|
+        li_id = update['time'].gsub(/ /, '_').gsub(/;/, '_').gsub(/:/, '_').gsub(/-/, '_')
+        @updates += "<li id='#{li_id}'><div><b>#{update['title']}</b> <span>(#{Time.parse(update['time']).strftime('%H:%M')})</span></div>#{update['text']}</li>"
+      end
+    end
     
     respond_to do |format|
       format.html
